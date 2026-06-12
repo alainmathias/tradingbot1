4c309e27f414ca68700d0e6678305698d4131a11
alainmathias <123120734+alainmathias@users.noreply.github.com>
2026-06-12 10:33:19 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,22 @@
+{
+  "name": "tradingbot",
+  "version": "1.0.0",
+  "description": "",
+  "main": "src/bot.js",
+  "scripts": {
+    "start": "node src/bot.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "type": "commonjs",
+  "dependencies": {
+    "axios": "^1.6.8",
+    "binance-api-node": "^0.13.9",
+    "dotenv": "^17.4.2",
+    "firebase-admin": "^13.10.0",
+    "node-telegram-bot-api": "^0.67.0",
+    "technicalindicators": "^3.1.0"
+  }
+}
